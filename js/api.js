0078a438568cd59497b7f53335476988f8894c6e
Moon-da-young <812319@naver.com>
2024-05-13 12:44:28 +0900

    forecast section

--- js/api.js
+++ js/api.js
@@ -3,12 +3,15 @@ const api_key = 'f934dc3a3e22bd52b408d047f49324bc';
 export const url = {
   //endpoint url
   currentWeather(lat, lon) {
-    return `https://api.openweathermap.org/data/2.5/weather
-    lat=${lat}&lon=${lon}&units=metric`;
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
   },
 
   geocode(loca) {
     return `https://api.openweathermap.org/geo/1.0/direct?q=${loca}&limit=5`;
+  },
+
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric`;
   },
 };
 
--- js/route.js
+++ js/route.js
@@ -1,7 +1,47 @@
+import { updateWeather } from './main.js';
+
 const defaultLocation = '#/weather?lat=37.55585&lan=126.9198'; //어메이징농카이 위치
 
-window.navigator.geolocation.getCurrentPosition((position) => {
-  const { latitude, longitude } = position.coords;
+const currentLocation = function () {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      updateWeather(latitude, longitude);
+    },
+    (error) => {
+      window.location.hash = defaultLocation;
+    }
+  );
+};
 
-  console.log(latitude, longitude);
+/**
+ *
+ * @param {string} query Searched location query e.g. city name
+ */
+const searchedLocation = (query) => updateWeather(...query.split('&'));
+// updateWeather("lat=37.51712", "lon=126.7159")
+
+const routes = new Map([
+  ['/current-location', currentLocation],
+  ['/weather', searchedLocation],
+]);
+
+const checkHash = function () {
+  const requestURL = window.location.hash.slice(1);
+  // console.log(requestURL);
+  const [route, query] = requestURL.includes
+    ? requestURL.split('?')
+    : [requestURL, ''];
+  routes.get(route) ? routes.get(route)(query) : 'Error 404';
+};
+
+window.addEventListener('hashchange', checkHash); // https://writingdeveloper.tistory.com/219
+window.addEventListener('load', function () {
+  if (!window.location.hash) {
+    window.location.hash = '#/current-location';
+  } else {
+    checkHash();
+  }
+
+  console.log(window.location.hash);
 });
